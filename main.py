edfd7c9dafa2e56fb602269557746924cd7da447
vladosfpv <theglasstea@gmail.com>
2020-01-25 23:34:23 +0300

    sas

--- main.py
+++ main.py
@@ -8,23 +8,22 @@ class Asteroid(pygame.sprite.Sprite):
 
         self.image = scale(pygame.image.load("images/asteroid.png"), (50, 50))
         self.rect = pygame.Rect(x, y, 50, 50)
-        self.yvel = 5
+        self.yvel = 0
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def update(self):
-        self.rect.y += self.yvel
         if self.rect.y > 900:
             self.kill()
 
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-
         self.rect = pygame.Rect(x, y, 50, 100)
         self.image = scale(pygame.image.load("images/car.png"), (100, 200))
         self.xvel = 0
+        self.yvel = 0
         # добавим кораблю здоровье
         self.life = 100
 
@@ -32,17 +31,37 @@ class Spaceship(pygame.sprite.Sprite):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
     # добавим группу с астероидами в обновление координат корабля
-    def update(self, left, right, asteroids):
-        if(self.rect.x > 300):
-            if left:
-                self.xvel -= 5
+    def update(self, left, right, up, down, asteroids):
+        if left:
+            self.xvel -= 5
         # если нажата клавиша вправо, увеличиваем скорость
-        if (self.rect.x < 850):
-            if right:
-                self.xvel += 5
+        if right:
+            self.xvel += 5
+        if up:
+            self.yvel -= 5
+        if down:
+            self.yvel += 5
         # если ничего не нажато - тормозим
-            if not (left or right):
-                self.xvel = 0
+        if not (left or right or down or up):
+            # выравнивание по иксу
+            if self.xvel > 0:
+                self.xvel -= 5
+            elif self.xvel < 0:
+                self.xvel += 5
+            # выравнивание по игрику
+            if self.yvel < 0:
+                self.yvel += 5
+            if self.yvel > 0:
+                self.yvel -= 5
+            # автостопы
+        if self.rect.x <= 210:
+            self.xvel = 5
+        if self.rect.x >= 970:
+            self.xvel = -5
+        if self.rect.y <= 0:
+            self.yvel = +5
+        if self.rect.y >= 1024:
+            self.yvel = -5
         # изменяем координаты на скорость
 
 
@@ -55,6 +74,7 @@ class Spaceship(pygame.sprite.Sprite):
                 self.life -= 10
 
         self.rect.x += self.xvel
+        self.rect.y += self.yvel
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 1024))
@@ -66,6 +86,8 @@ ship = Spaceship(300, 300)
 
 left = False
 right = False
+up = False
+down = False
 
 asteroids = pygame.sprite.Group()
 
@@ -74,7 +96,7 @@ pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 30)
 
 dist = 0
-
+#основной цикл
 while True:
     if(ship.rect.x < 1):
         ship.rect.x = 1
@@ -90,11 +112,20 @@ while True:
             left = True
         if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
             right = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
+            up = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_DOWN:
+            down = True
+
 
         if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
             left = False
         if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
             right = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_UP:
+            up = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_DOWN:
+            down = False
 
         if e.type == pygame.QUIT:
             raise SystemExit("QUIT")
@@ -102,7 +133,7 @@ while True:
     screen.blit(sky, (0, 0))
 
     # добавим группу астероидов в параметры
-    ship.update(left, right, asteroids)
+    ship.update(left, right, up, down, asteroids)
     ship.draw(screen)
 
     for asteroid in asteroids:
