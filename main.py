7a752deb076baf7f1fe00ba92bc5c6cc5699346c
Геннадий <mrsnegovikl2015@gmail.com>
2020-01-27 11:42:48 +0300

    Merge branch 'master' of C:\Users\Геннадий\PycharmProjects\QwertyProj with conflicts.

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import pygame
 import random
 from pygame.transform import scale
 
+class Road(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(x, y, 1280, 1024)
+        self.image = scale(pygame.image.load("images/road.png"), (1280, 1024))
+        self.yvel = 0
+
+    def update(self):
+        self.rect.y += 20
+        if self.rect.y > 1024:
+            self.kill()
+            self.rect.y = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y - 1024))
+
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -9,7 +26,7 @@ class Asteroid(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, 50, 50)
         self.yvel = 5
     def update(self):
-        self.rect.y += 5
+        self.rect.y += 20
         if self.rect.y > 900:
             self.kill()
     def draw(self, screen):
@@ -22,23 +39,23 @@ class Player1(pygame.sprite.Sprite):
         self.image = scale(pygame.image.load("images/car.png"), (100, 200))
         self.xvel = 0
         self.yvel = 0
-        self.score = 0
+        self.life = 100
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
     # добавим группу с астероидами в обновление координат корабля
-    def update(self, left1, right1, up1, down1, asteroids, another):
-        if left1:
+    def update(self, left, right, up, down, asteroids):
+        if left:
             self.xvel -= 5
         # если нажата клавиша вправо, увеличиваем скорость
-        if right1:
+        if right:
             self.xvel += 5
-        if up1:
+        if up:
             self.yvel -= 5
-        if down1:
+        if down:
             self.yvel += 5
         # если ничего не нажато - тормозим
-        if not (left1 or right1 or down1 or up1):
+        if not (left or right or down or up):
             # выравнивание по иксу
             if self.xvel > 0:
                 self.xvel -= 5
@@ -59,43 +76,42 @@ class Player1(pygame.sprite.Sprite):
         if self.rect.y >= 1024:
             self.yvel = -5
         # изменяем координаты на скорость
+
+
+        # для каждого астероида
         for asteroid in asteroids:
             # если область, занимаемая астероидом пересекает область корабля
             if self.rect.colliderect(asteroid.rect):
-                # уменьшаем очки
+                # уменьшаем жизнь
                 asteroid.kill()
-                if self.score >= 10:
-                    self.score -= 10
-        if self.rect.colliderect(another.rect):
-            self.xvel = -self.xvel
+                self.life -= 10
+
         self.rect.x += self.xvel
         self.rect.y += self.yvel
-
-
 class Player2(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(x, y, 60, 195)
+        self.rect = pygame.Rect(x, y, 50, 195)
         self.image = scale(pygame.image.load("images/car.png"), (100, 200))
         self.xvel = 0
         self.yvel = 0
-        self.score = 0
+        self.life = 100
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
     # добавим группу с астероидами в обновление координат корабля
-    def update(self, left2, right2, up2, down2, asteroids,another):
-        if left2:
+    def update(self, left, right, up, down, asteroids):
+        if left:
             self.xvel -= 5
         # если нажата клавиша вправо, увеличиваем скорость
-        if right2:
+        if right:
             self.xvel += 5
-        if up2:
+        if up:
             self.yvel -= 5
-        if down2:
+        if down:
             self.yvel += 5
         # если ничего не нажато - тормозим
-        if not (left2 or right2 or down2 or up2):
+        if not (left or right or down or up):
             # выравнивание по иксу
             if self.xvel > 0:
                 self.xvel -= 5
@@ -115,57 +131,73 @@ class Player2(pygame.sprite.Sprite):
             self.yvel = +5
         if self.rect.y >= 1024:
             self.yvel = -5
-        # изменяем координаты на скорости
+        # изменяем координаты на скорость
+
+
         # для каждого астероида
         for asteroid in asteroids:
             # если область, занимаемая астероидом пересекает область корабля
             if self.rect.colliderect(asteroid.rect):
                 # уменьшаем жизнь
                 asteroid.kill()
-                if self.score >= 10:
-                    self.score -= 10
-        if self.rect.colliderect(another.rect):
-            self.xvel = -self.xvel
+                self.life -= 10
+
         self.rect.x += self.xvel
         self.rect.y += self.yvel
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 1024))
-pygame.display.set_caption("Asteroids")
+pygame.display.set_caption("GONKA")
+road1 = Road(0, 0)
+road2 = Road(0, -1024)
+ship = Player1(300, 300)
 
-sky = scale(pygame.image.load("images/road.png"), (1280, 1024))
 
-ship1 = Player1(300, 300)
-ship2 = Player2(500, 300)
 
 left1 = False
 right1 = False
 up1 = False
 down1 = False
-left2 = False
-right2 = False
-up2 = False
-down2 = False
 
 asteroids = pygame.sprite.Group()
 
 # загрузим системный шрифт
 pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 30)
-
+dist = 0
 #основной цикл
 while True:
-    if(ship1.rect.x < 1):
-        ship1.rect.x = 1
-    if (ship2.rect.x < 1):
-        ship2.rect.x = 1
-    if random.randint(1, 1000) > 950:
+    road1.update()
+    road1.draw(screen)
+
+    if(ship.rect.x < 1):
+        ship.rect.x = 1
+    if random.randint(1, 150) > 149:
         asteroid_x = random.randint(325, 900)
-        asteroid_y = 0
+        asteroid_y = -100
         asteroid = Asteroid(asteroid_x, asteroid_y)
         asteroids.add(asteroid)
 
     for e in pygame.event.get():
+
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
+            left1 = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
+            right1 = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
+            up1 = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_DOWN:
+            down1 = True
+
+
+        if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
+            left1 = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
+            right1 = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_UP:
+            up1 = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_DOWN:
+            down1 = False
 
         if e.type == pygame.KEYDOWN and e.key == pygame.K_a:
             left1 = True
@@ -176,6 +208,7 @@ while True:
         if e.type == pygame.KEYDOWN and e.key == pygame.K_s:
             down1 = True
 
+
         if e.type == pygame.KEYUP and e.key == pygame.K_a:
             left1 = False
         if e.type == pygame.KEYUP and e.key == pygame.K_d:
@@ -185,42 +218,20 @@ while True:
         if e.type == pygame.KEYUP and e.key == pygame.K_s:
             down1 = False
 
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
-            left2 = True
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
-            right2 = True
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
-            up2 = True
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_DOWN:
-            down2 = True
-
-        if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
-            left2 = False
-        if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
-            right2 = False
-        if e.type == pygame.KEYUP and e.key == pygame.K_UP:
-            up2 = False
-        if e.type == pygame.KEYUP and e.key == pygame.K_DOWN:
-            down2 = False
-
         if e.type == pygame.QUIT:
             raise SystemExit("QUIT")
 
-    screen.blit(sky, (0, 0))
-
     # добавим группу астероидов в параметры
-    ship1.update(left1, right1, up1, down1, asteroids, ship2)
-    ship1.draw(screen)
-    ship2.update(left2, right2, up2, down2, asteroids, ship1)
-    ship2.draw(screen)
+    ship.update(left1, right1, up1, down1, asteroids)
+    ship.draw(screen)
 
-    for asteroid in asteroids:
-        asteroid.update()
-        asteroid.draw(screen)
+    asteroids.update()
+    asteroids.draw(screen)
 
     # выведем жизнь на экран белым цветом
-    score1 = font.render(f'player 1: {ship1.score}', False, (255, 255, 255))
-    score2 = font.render(f'player 2: {ship2.score}', False, (255, 255, 255))
-    screen.blit(score1, (20, 20))
-    screen.blit(score2, (1000, 20))
+    dist = dist + 1
+    life = font.render(f'Score: {ship.life}', False, (0, 0, 0))
+    distOUT = font.render(f'dist: {dist}', False, (0, 0, 0))
+    screen.blit(life, (20, 20))
+    screen.blit(distOUT, (1100, 20))
     pygame.display.update()
